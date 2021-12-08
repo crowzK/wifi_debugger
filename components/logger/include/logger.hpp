1320d73d41688171bfd2f59774d3d338fbb8cc93
CrowzK <craven@crowz.kr>
2021-12-07 21:35:38 -0500

    remove telnet

--- components/logger/include/logger.hpp
+++ components/logger/include/logger.hpp
@@ -1,50 +1,6 @@
 #ifndef TELNET_HPP
 #define TELNET_HPP
 
-#include <memory>
-#include <vector>
-#include <thread>
-#include <atomic>
-#include <mutex>
-
-class TelnetClient
-{
-public:
-    TelnetClient(int socket);
-    ~TelnetClient();
-
-    //! \brief It handles the recv data from the telnet client
-    //! \note it will send the data to the UART
-    void tcpRxTask();
-
-protected:
-    const int socket;
-    void * telnet;
-    std::atomic<bool> run;
-
-    std::thread txHandle;
-    std::recursive_mutex mutex;
-
-    //! \brief It handles the recv data from the UART
-    //! \note it will send the data to the Telnet client
-    void tcpTxTask();
-
-    void sendUart(const char *buffer, size_t size);
-    void sendToClient(const char *buffer, size_t size);
-    static void eventHandler(void *telnet, void *ev, TelnetClient *tcpClient);
-};
-
-class Telnet
-{
-public:
-    Telnet();
-    ~Telnet();
-
-protected:
-    std::thread threadHandle;
-    void thread();
-};
-
 void start_telnet();
 
 #endif
