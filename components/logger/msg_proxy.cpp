4844ce594724724dcbce446687bcad549f9d0e6d
Yudoc Kim <craven@crowz.kr>
2023-08-19 22:01:36 -0400

    Support ESP-IDF 5.1 (#40)

--- components/logger/msg_proxy.cpp
+++ components/logger/msg_proxy.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #include <iostream>
 #include <sstream>
 #include <regex>
+#include <string.h>
 #include "msg_proxy.hpp"
 #include "esp_log.h"
 
