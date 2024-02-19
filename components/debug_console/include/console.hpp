093eb1004d9393954257138939650d2a0875ad8b
CrowzK <craven@crowz.kr>
2024-02-18 22:35:30 -0500

    suport 5.2

--- components/debug_console/include/console.hpp
+++ components/debug_console/include/console.hpp
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #include <list>
 #include <vector>
 #include <mutex>
+#include <string>
 #include "task.hpp"
 
 class UartByPass;
